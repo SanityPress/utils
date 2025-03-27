9d973941d66716e01c4996e85783296d63c183de
Mitchell Christ <mitchell@human.marketing>
2025-03-27 07:42:18 -0700

    remove pro

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-export { sanitypress } from 'sanity-plugin-sanitypress'
-
 export { singleton, group, directory } from './structure'
 
 export { alignItems, textAlign, getBlockText } from './schema'
